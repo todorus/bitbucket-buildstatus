c1d06599bb37620a3028711568bd72b248fa3496
Theo van den Bogaart <todorus@todorus.com>
2015-12-30 23:19:53 +0100

    committing some WIP, moving development to other system

---
+++ src/test/java/com/todorus/bitbucketbuildstatus/control/BuildStatusControllerTest.java
@@ -0,0 +1,16 @@
+package com.todorus.bitbucketbuildstatus.control;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by todorus on 10/12/15.
+ */
+public class BuildStatusControllerTest {
+
+  @Test
+  public void testPostStatus() throws Exception {
+
+  }
+}
